e106ea7767c0875762b06de822f9b892ba668f3a
Divy611 <divy.parikh@hotmail.com>
2024-03-07 15:49:10 +0530

    Face recordings moved to images

--- face_recognition.py
+++ face_recognition.py
@@ -2,22 +2,26 @@ import numpy as np
 import cv2
 import os
 
+
 def distance(v1, v2):
-	return np.sqrt(((v1-v2)**2).sum())
+    return np.sqrt(((v1-v2)**2).sum())
+
 
 def knn(train, test, k=5):
-	dist = []
-	for i in range(train.shape[0]):
-		ix = train[i, :-1]
-		iy = train[i, -1]
-		d = distance(test, ix)
-		dist.append([d, iy])
+    dist = []
+    for i in range(train.shape[0]):
+        ix = train[i, :-1]
+        iy = train[i, -1]
+        d = distance(test, ix)
+        dist.append([d, iy])
 
-	dk = sorted(dist, key=lambda x: x[0])[:k]
-	labels = np.array(dk)[:, -1]
-	output = np.unique(labels, return_counts=True)
-	index = np.argmax(output[1])
-	return output[0][index]
+    dk = sorted(dist, key=lambda x: x[0])[:k]
+    labels = np.array(dk)[:, -1]
+    output = np.unique(labels, return_counts=True)
+    index = np.argmax(output[1])
+    return output[0][index]
+
+
 cap = cv2.VideoCapture(0)
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 dataset_path = "./face_dataset/"
@@ -29,13 +33,18 @@ names = {}
 
 
 for fx in os.listdir(dataset_path):
-	if fx.endswith('.npy'):
-		names[class_id] = fx[:-4]
-		data_item = np.load(dataset_path + fx)
-		face_data.append(data_item)
-		target = class_id * np.ones((data_item.shape[0],))
-		class_id += 1
-		labels.append(target)
+    if fx.endswith('.npy'):
+        names[class_id] = fx[:-4]
+        data_item = np.load(dataset_path + fx)
+        face_data.append(data_item)
+        target = class_id * np.ones((data_item.shape[0],))
+        class_id += 1
+        labels.append(target)
+    elif fx.endswith(".jpg") or fx.endswith(".jpeg") or fx.endswith(".png"):
+        name = fx.split('_')[0]
+        if name not in names:
+            names[name] = class_id
+            class_id += 1
 
 face_dataset = np.concatenate(face_data, axis=0)
 face_labels = np.concatenate(labels, axis=0).reshape((-1, 1))
@@ -47,24 +56,25 @@ print(trainset.shape)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 while True:
-	ret, frame = cap.read()
-	if ret == False:
-		continue
-	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    ret, frame = cap.read()
+    if ret == False:
+        continue
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-	for face in faces:
-		x, y, w, h = face
-		offset = 5
-		face_section = frame[y-offset:y+h+offset, x-offset:x+w+offset]
-		face_section = cv2.resize(face_section, (100, 100))
-		out = knn(trainset, face_section.flatten())
-		cv2.putText(frame, names[int(out)],(x,y-10), cv2.FONT_ITALIC, 1,(0,0,0),2,cv2.LINE_AA)
-		cv2.rectangle(frame, (x,y), (x+w,y+h), (0,255,255), 2)
+    for face in faces:
+        x, y, w, h = face
+        offset = 5
+        face_section = frame[y-offset:y+h+offset, x-offset:x+w+offset]
+        face_section = cv2.resize(face_section, (100, 100))
+        out = knn(trainset, face_section.flatten())
+        cv2.putText(frame, names[int(out)], (x, y-10),
+                    cv2.FONT_ITALIC, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 255), 2)
 
-	cv2.imshow("Faces", frame)
-	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+    cv2.imshow("Faces", frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
